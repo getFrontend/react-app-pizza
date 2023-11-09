187666855b094077a3943bcaf47ce67e044c927c
Sergey UP <sergey10goals@gmail.com>
2023-11-09 10:18:59 +0200

    update Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import Button from './components/Button/Button'
+import { MouseEvent, useState } from 'react';
+import './App.css';
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
 
 function App() {
-  const [counter, setCounter] = useState<number>(0)
+  const [counter, setCounter] = useState<number>(0);
 
   const addCounter = (e: MouseEvent) => {
     console.log(e);
-  }
+  };
 
   return (
     <>
-      <Button onClick={() => addCounter}>Click</Button>
+      <Button onClick={addCounter}>Click</Button>
+      <Button appearence='big' onClick={addCounter}>Click</Button>
+      <Input placeholder='Type the text please' />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
