405f80727dac5bcbe91516f32c57f92eb4f0e97a
Sergey UP <sergey10goals@gmail.com>
2023-11-10 17:03:59 +0200

    add pages & navLink & menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,7 @@ import { MouseEvent, useState } from 'react';
 import './App.css';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Cart } from './pages/Cart/Cart';
-import { Menu } from './pages/Menu/Menu';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Menu />
-  },
-  {
-    path: '/cart',
-    element: <Cart />
-  }
-]);
+import { RouterProvider, createBrowserRouter, Link } from 'react-router-dom';
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -30,8 +17,8 @@ function App() {
       <Button appearence='big' onClick={addCounter}>Click</Button>
       <Input placeholder='Type the text please' />
       <div>
-        <a href="/">Menu</a>
-        <a href="/cart">Cart</a>
+        <Link to="/">Menu</Link>
+        <Link to="/cart">Cart</Link>
       </div>
       <RouterProvider router={router} />
     </>
