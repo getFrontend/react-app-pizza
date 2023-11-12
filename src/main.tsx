9d84eb6b1c67a97d6b5fa724371af2b6e61ba783
Sergey UP <sergey10goals@gmail.com>
2023-11-12 18:03:24 +0200

    add lazy & Suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Menu } from './pages/Menu/Menu.tsx';
 import { Cart } from './pages/Cart/Cart.tsx';
-import { Error } from './pages/Error/Error.tsx';
+import { Error as ErrorPage } from './pages/Error/Error.tsx';
 import { Layout } from './layout/Menu/MenuLayout.tsx';
 import { Product } from './pages/Product/Product.tsx';
 import { PREFIX } from './helpers/API.ts';
 import axios from 'axios';
+
+const Menu = lazy(() => import('./pages/Menu/Menu'));
 
 const router = createBrowserRouter([
   {
@@ -17,7 +18,10 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Menu />
+        element:
+          <Suspense fallback={<>Загрузка...</>}>
+            <Menu />
+          </Suspense>
       },
       {
         path: '/cart',
@@ -36,7 +40,7 @@ const router = createBrowserRouter([
   },
   {
     path: '*',
-    element: <Error />
+    element: <ErrorPage />
   }
 ]);
 
