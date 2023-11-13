6addd8cd50daf6a4f4f22649ec24c8515663cf78
Sergey UP <sergey10goals@gmail.com>
2023-11-13 18:25:29 +0200

    update Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,34 @@
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import Headling from '../../components/Headling/Headling';
+import Input from '../../components/Input/Input';
+import styles from './Login.module.css';
+import { FormEvent } from 'react';
+
 export function Login() {
+  const submit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(event);
+  };
+
   return (
-    <>Login</>
+    <div className={styles['login']} onSubmit={submit}>
+      <Headling>Вход</Headling>
+      <form className={styles['form']}>
+        <div className={styles['field']}>
+          <label htmlFor="email">Ваш email:</label>
+          <Input id="email" type="email" placeholder="Email" autoComplete="off" />
+        </div>
+        <div className={styles['field']}>
+          <label htmlFor="password">Ваш пароль:</label>
+          <Input id="password" type="password" placeholder='Password' />
+        </div>
+        <Button appearence="big">Войти</Button>
+      </form>
+      <div className={styles['links']}>
+        <div>Нет аккаунта?</div>
+        <Link to="/auth/register">Зарегистрироваться</Link>
+      </div>
+    </div>
   );
 }
