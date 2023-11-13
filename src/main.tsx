bcfff757bcceb9d73f3e15e5cd04771b586e2721
Sergey UP <sergey10goals@gmail.com>
2023-11-13 17:19:07 +0200

    last update

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import { Layout } from './layout/Menu/MenuLayout.tsx';
 import { Product } from './pages/Product/Product.tsx';
 import { PREFIX } from './helpers/API.ts';
 import axios from 'axios';
+import { AuthLayout } from './layout/Auth/AuthLayout.tsx';
+import { Login } from './pages/Login/Login.tsx';
+import { Register } from './pages/Register/Register.tsx';
 
 const Menu = lazy(() => import('./pages/Menu/Menu'));
 
@@ -35,10 +38,21 @@ const router = createBrowserRouter([
           return defer({
             data: await axios.get(`${PREFIX}/products/${params.id}`)
           });
-
-          // const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
-          // return data;
         }
+      }
+    ]
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
       }
     ]
   },
