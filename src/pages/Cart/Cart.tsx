4e81419260708447cb83b5c275e017a35fa39d9d
Sergey UP <sergey10goals@gmail.com>
2023-11-14 23:19:40 +0200

    update Cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -6,10 +6,21 @@ import { useEffect, useState } from 'react';
 import { Product } from '../../interfaces/product.interface';
 import axios from 'axios';
 import { PREFIX } from '../../helpers/API';
+import styles from './Cart.module.css';
+
+const DELIVERY_PRICE = 99;
 
 export function Cart() {
 	const [cartProducts, setCartProducts] = useState<Product[]>([]);
 	const items = useSelector((s: RootState) => s.cart.items);
+	const total =
+		items.map(i => {
+			const product = cartProducts.find(p => p.id === i.id);
+			if (!product) {
+				return 0;
+			}
+			return i.count * product.price;
+		}).reduce((acc, i) => acc += i, 0);
 
 	const getItem = async (id: number) => {
 		const { data } = await axios.get(`${PREFIX}/products/${id}`);
@@ -27,7 +38,7 @@ export function Cart() {
 
 	return (
 		<>
-			<Headling>Корзина</Headling>
+			<Headling className={styles['headling']}>Корзина</Headling>
 			{items.map(item => {
 				const product = cartProducts.find(p => p.id === item.id);
 				if (!product) {
@@ -35,6 +46,20 @@ export function Cart() {
 				}
 				return <CartItem key={product.id} count={item.count} {...product} />;
 			})}
+			<div className={styles['line']}>
+				<div className={styles['text']}>Сумма</div>
+				<div className={styles['price']}>{total}&nbsp;<span>₴</span></div>
+			</div>
+			<hr className={styles['hr']} />
+			<div className={styles['line']}>
+				<div className={styles['text']}>Доставка</div>
+				<div className={styles['price']}>{total > 1000 ? 0 : DELIVERY_PRICE}&nbsp;<span>₴</span></div>
+			</div>
+			<hr className={styles['hr']} />
+			<div className={styles['line']}>
+				<div className={styles['text']}>Итого с учётом доставки</div>
+				<div className={styles['price']}>{total + DELIVERY_PRICE}&nbsp;<span>₴</span></div>
+			</div>
 		</>
 	);
 }
